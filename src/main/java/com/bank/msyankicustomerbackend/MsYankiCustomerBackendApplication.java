56de586138ba1b534a2e22d0e9cd26d50ebbc675
Amado David Ortiz Gallardo <aortigal@emeal.nttdata.com>
2022-08-26 15:03:58 -0500

    add estructure base

---
+++ src/main/java/com/bank/msyankicustomerbackend/MsYankiCustomerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bank.msyankicustomerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsYankiCustomerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsYankiCustomerBackendApplication.class, args);
+	}
+
+}
